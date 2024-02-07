077ec59b73b0ffc07fc086d7d28eca0d55bd35f2
Maxwell <130581484+Maxw311nyimbili@users.noreply.github.com>
2024-02-07 18:46:18 +0000

    added a function for emailing the captured image to an email address.

--- emailing.py
+++ emailing.py
@@ -1,2 +1,26 @@
-def send_email():
-    print("Email sent!")
+import smtplib
+import imghdr # gives meta-data about the image
+from email.message import EmailMessage
+
+PASSWORD = "zlur zkbo beys ooek"
+SENDER = "zisacompanyltd@gmail.com"
+RECIEVER = "zisacompanyltd@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    # body of the email
+    email_message.set_content("Hey, we just got a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECIEVER, email_message.as_string())
+
+
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
 import time
+import glob
+import os
 from emailing import send_email
+from threading import Thread
+
+
 # the class VideoCapture() is used to capture a video, and putting zero in the
 # parenthesis means that you want to use the primary camera on the device. Putting a one in the
 # parenthesis means that you want to use a secondary camera to capture the video.
@@ -11,11 +16,24 @@ from emailing import send_email
 
 # to show the video, use a method called imshow(), and it should be placed inside a while-loop
 
+
+# THREADING IN PYTHON
+# The program is working successfully, but because it is executing different parts of the program
+# at once, the webcam video freeze(slows down), to ensure that it both sends an email and displays
+# footage on the webcam. To avoid this, we use what is known as Threading.
 video = cv2.VideoCapture(0)
 time.sleep(1)  # sets the delay to one second
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    image_list = glob.glob("images/*")
+    for image in image_list:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -31,7 +49,7 @@ while True:
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    cv2.imshow("My Video", dil_frame)  # shows a picture of the thing that entered the camera view, because the
+    # cv2.imshow("My Video", dil_frame)  # shows a picture of the thing that entered the camera view, because the
     # that frame is different from the "first_frame" that was initially captured
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -41,19 +59,37 @@ while True:
         if cv2.contourArea(contour) < 10000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 355, 0), 3)
         if rectangle.any():
             status = 1
+
+            cv2.imwrite(f"images/{count}.png", frame)  # storing the image
+            count = count + 1
+            all_images = glob.glob("images/*.png")  # getting image name & directory and putting it in a list
+            index = int(len(all_images)/2)  # index of the middle image
+            images_with_object = all_images[index]
+
     # send email when the image exists camera view
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(images_with_object, ))
+        email_thread.daemon = True
+
+        # clean the image folder after the email has been sent
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+
 
     cv2.imshow("video", frame)
     key = cv2.waitKey(1)
 
     if key == ord("q"):
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.start()
         break
+
 video.release()
